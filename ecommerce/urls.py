9c88b76b73b86e42c76880920d2299578df5eb92
kira <contacto@contaco.com>
2020-05-04 12:22:33 -0500

    checkout for logged users

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('',include("store.urls"))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
