305e1d0d7a58b2b8166ae669d4cae00ec87182ee
kira <contacto@contaco.com>
2020-05-10 20:04:13 -0500

    update cart logo order

--- store/views.py
+++ store/views.py
@@ -6,8 +6,16 @@ from django.http import JsonResponse
 import json
 
 def store(request):
+    if request.user.is_authenticated:
+        client = request.user.customer
+        order, created = Order.objects.get_or_create(customer=client, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        order = {'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products,'cartItems':cartItems}
     return render(request,"store/store.html", context)
 
 def cart(request):
@@ -15,10 +23,12 @@ def cart(request):
         client = request.user.customer
         order, created = Order.objects.get_or_create(customer=client, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total':0,'get_cart_items':0}
-    context = {'items':items,'order':order}
+        cartItems = order['get_cart_items']
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,"store/cart.html", context)
 
 
@@ -27,10 +37,12 @@ def checkout(request):
         client = request.user.customer
         order, created = Order.objects.get_or_create(customer=client, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         order = {'get_cart_total':0,'get_cart_items':0}
+        cartItems = order['get_cart_items']
         items = []
-    context = {'items':items,'order':order}
+    context = {'items':items,'order':order,'cartItems':cartItems}
     return render(request,"store/checkout.html",context)
 
 def updateItem(request):
